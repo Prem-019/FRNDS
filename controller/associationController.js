241b5a46099791cdfedca249e9b505944d35da4b
PREM <PREMGOSWAMI2892@GMAIL.COM>
2023-03-24 14:26:55 -0400

    Association update and delete functionality added

--- controller/associationController.js
+++ controller/associationController.js
@@ -1,7 +1,7 @@
 import Association from '../models/associationModel.js'
 import asyncHandler from 'express-async-handler'
 
-// @desc   Create Event
+// @desc   Create Association
 // @Route  POST /api/associations/create
 // @access PRIVATE (ADMIN)
 const createAssociation = asyncHandler(async (req, res) => {
@@ -27,7 +27,7 @@ const createAssociation = asyncHandler(async (req, res) => {
   res.json(association)
 })
 
-// @desc   GET Events
+// @desc   GET Association
 // @Route  GET /api/associations/create
 // @access PRIVATE (Auth)
 const getAssociations = asyncHandler(async (req, res) => {
@@ -36,4 +36,53 @@ const getAssociations = asyncHandler(async (req, res) => {
   res.json(associations)
 })
 
-export { createAssociation, getAssociations }
+// @desc   PUT Association
+// @Route  PUT /api/associations/update/:id
+// @access PRIVATE (Admin)
+const updateAssociation = asyncHandler(async (req, res) => {
+  const association = await Association.findById(req.params.id)
+
+  if (association) {
+    const { name, city, email, website, country, contact, rating } = req.body
+
+    association.name = name || association.name
+    association.city = city || association.city
+    association.email = email || association.email
+    association.website = website || association.website
+    association.country = country || association.country
+    association.contact = contact || association.contact
+    association.rating = rating || association.rating
+
+    await association.save()
+
+    res.status(201)
+    res.json(association)
+  } else {
+    res.status(404)
+    throw new Error('Association not found!')
+  }
+})
+
+// @desc   DELETE Association
+// @Route  DELETE /api/associations/delete/:id
+// @access PRIVATE (Admin)
+const deleteAssociation = asyncHandler(async (req, res) => {
+  const assocation = await Association.findById(req.params.id)
+
+  if (assocation) {
+    await assocation.remove()
+
+    res.status(201)
+    res.json({ message: 'Association removed Successfully' })
+  } else {
+    res.status(404)
+    throw new Error('Association not found!')
+  }
+})
+
+export {
+  createAssociation,
+  getAssociations,
+  updateAssociation,
+  deleteAssociation,
+}
--- routes/associationRoutes.js
+++ routes/associationRoutes.js
@@ -2,12 +2,16 @@ import express from 'express'
 import { protect, adminCheck } from '../middleware/authMiddleware.js'
 import {
   createAssociation,
+  deleteAssociation,
   getAssociations,
+  updateAssociation,
 } from '../controller/associationController.js'
 
 const router = express.Router()
 
 router.route('/').get(protect, getAssociations)
 router.route('/create').post(protect, adminCheck, createAssociation)
+router.route('/update/:id').put(protect, adminCheck, updateAssociation)
+router.route('/delete/:id').delete(protect, adminCheck, deleteAssociation)
 
 export default router
